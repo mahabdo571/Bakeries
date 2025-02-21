fb7baf1e8aa7e594c6620905a7f4fa875b1444a9
mahmoud abdo <mah.abdo571@gmail.com>
2025-02-21 12:55:06 +0200

    انشاء خدمة المنتجات التي يتم انتاجها في الواجهة الامامية ببلازور
    اسم الكلاس
    ProductService
    
    التهيئة الاساسية للعمليات اضافة حذف تعديل وجلب الكل

---
+++ bakerbalzorwebassembly/Services/ProductService.cs
@@ -0,0 +1,62 @@
+﻿using bakerbalzorwebassembly.Models;
+using Business.Shared.DTOs;
+using System.Net.Http.Json;
+
+namespace bakerbalzorwebassembly.Services
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<ProductDTO>> GetAllAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<ProductDTO>>("api/Product/All");
+            return response ?? new List<ProductDTO>();
+        }
+
+
+
+
+        public async Task<ProductDTO> AddAsync(ProductDTO newModel)
+        {
+            Console.WriteLine(newModel);
+            var response = await _httpClient.PostAsJsonAsync("api/Product", newModel);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                return await response.Content.ReadFromJsonAsync<ProductDTO>();
+            }
+            return null;
+        }
+
+
+        public async Task<bool> UpdateAsync(ProductDTO updetedModel)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/Product/{updetedModel.Id}", updetedModel);
+            return response.IsSuccessStatusCode;
+        }
+
+
+        public async Task<ApiError?> DeleteAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Product/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                var apiError = await response.Content.ReadFromJsonAsync<ApiError>();
+                if (apiError != null)
+                {
+                    return apiError;
+                }
+            }
+
+            return null;
+        }
+
+    }
+}
