2c3a986fa8c55cc03d2c70e6c27c665b583f7252
mahmoud abdo <mah.abdo571@gmail.com>
2025-04-05 17:54:04 +0300

    e

--- Bakeries.DataAccess/DesignTimeDbContextFactory.cs
+++ Bakeries.DataAccess/DesignTimeDbContextFactory.cs
@@ -20,7 +20,7 @@ namespace DAevthERP
 
             var configuration = new ConfigurationBuilder()
 
-              .SetBasePath("C:\\Programming works\\Bakeries\\Bakeries.API\\")//
+              .SetBasePath(@"C:\MyProgrammingWork\Bakeries\Bakeries.API\")//
                                                                              // .SetBasePath("C:\\devlop\\Bakeries\\Bakeries.API")//Directory.GetCurrentDirectory()
                 .AddJsonFile("appsettings.json")
                 .Build();
